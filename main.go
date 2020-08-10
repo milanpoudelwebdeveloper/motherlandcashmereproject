889ee95c552fa6c3573fb35173f7e29a55e4dba7
milanpoudel <milanwebdeveloper1@gmail.com>
2020-08-09 21:01:35 -0700

    Created our first web server

--- main.go
+++ main.go
@@ -1,8 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":8080", nil)
 
+}
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Welcome to my awesome site</h1>")
 }
