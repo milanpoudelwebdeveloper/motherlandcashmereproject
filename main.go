b66f8c3eb9cf7cbfb6b14741a15571a83d50e9b6
milanpoudel <milanwebdeveloper1@gmail.com>
2020-08-10 01:50:18 -0700

    Added a contact page

--- main.go
+++ main.go
@@ -6,12 +6,18 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/dogs/", handlerFunc)
+	http.HandleFunc("/", handlerFunc)
 	http.ListenAndServe(":8080", nil)
 
 }
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain")
-	fmt.Fprint(w, "<h1>Welcome to our copy site</h1>")
+	w.Header().Set("Content-Type", "text/html")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, `<h1>Welcome to te awesome site`)
+	}
+	if r.URL.Path == "/contact" {
+
+		fmt.Fprint(w, `To get in touch, please send an email to <a href="mailto:support@lenslocked.com">support@lenslocked.com</a>`)
+	}
 }
