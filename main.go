0f9288cc4f9d028057f5b9bb456d3c50fc0418b2
milanpoudel <milanwebdeveloper1@gmail.com>
2020-08-11 03:12:45 -0700

    Created our first basic home view

--- main.go
+++ main.go
@@ -3,13 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"text/template"
 
 	"github.com/gorilla/mux"
 )
 
+var homeTemplate *template.Template
+
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html charset=utf-8")
-	fmt.Fprint(w, `<h1>Welcome to the awesome site</h1>`)
+	if err := homeTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
@@ -18,6 +23,12 @@ func contact(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+
+	var err error
+	homeTemplate, err = template.ParseFiles("views/home.gohtml")
+	if err != nil {
+		panic(err)
+	}
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
