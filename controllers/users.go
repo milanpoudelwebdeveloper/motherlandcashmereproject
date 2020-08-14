19fb9e2d65c9746e69d93166cd111e00a8727d50
milanpoudel <milanwebdeveloper1@gmail.com>
2020-08-13 19:49:05 -0700

    Created the create() method on the users controller and updated our route to have specific HTTP verbs

--- controllers/users.go
+++ controllers/users.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 
 	"../views"
@@ -22,9 +23,19 @@ type Users struct {
 	NewView *views.View
 }
 
-//New is
+// //GET/signup
+
+//New is used to render the form where the user can create a new user account
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 	if err := u.NewView.Render(w, nil); err != nil {
 		panic(err)
 	}
+}
+
+//POST/signup
+
+//Create is used to create new user account
+//Create is used to process the signup form where users submits it
+func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "This is a temporary response")
 }
--- main.go
+++ main.go
@@ -36,9 +36,11 @@ func main() {
 	contactView = views.NewView("bootstrap", "views/contact.gohtml")
 	usersC := controllers.NewUsers()
 	r := mux.NewRouter()
-	r.HandleFunc("/", home)
-	r.HandleFunc("/contact", contact)
-	r.HandleFunc("/signup", usersC.New)
+	r.HandleFunc("/", home).Methods("GET")
+	r.HandleFunc("/contact", contact).Methods("GET")
+	r.HandleFunc("/signup", usersC.New).Methods("GET")
+	r.HandleFunc("/signup", usersC.Create).Methods("POST")
+
 	http.ListenAndServe(":8054", r)
 
 }
