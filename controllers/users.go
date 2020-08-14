03773440b00a21c44ca7f4be8cc6c43c3d6a54c6
milanpoudel <milanwebdeveloper1@gmail.com>
2020-08-14 04:32:18 -0700

    Added the schema package and used to parse our forms with the signup form type

--- controllers/users.go
+++ controllers/users.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/gorilla/schema"
+
 	"../views"
 )
 
@@ -32,6 +34,12 @@ func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+//SignupForm is
+type SignupForm struct {
+	Email    string `schema:"email"`
+	Password string `schema:"password"`
+}
+
 //POST/signup
 
 //Create is used to create new user account
@@ -40,10 +48,15 @@ func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
 		panic(err)
 	}
+	dec := schema.NewDecoder()
+	var form SignupForm
+	if err := dec.Decode(&form, r.PostForm); err != nil {
+		panic(err)
+	}
+	fmt.Fprintln(w, form)
 
 	fmt.Fprintln(w, r.PostForm["email"])
 	fmt.Fprintln(w, r.PostForm["password"])
-	fmt.Fprintln(w, "This is a temporary response")
 
 	//returns the first value from the given "email" and "password"
 	// fmt.Fprintln(w, r.PostFormValue("email"))
