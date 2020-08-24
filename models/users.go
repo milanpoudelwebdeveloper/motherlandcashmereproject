e9eb8e4d57471285725f9b804d8cda1c3947b95e
milanpoudel <milanwebdeveloper1@gmail.com>
2020-08-24 01:36:31 -0700

    Added a pepper to our UserService

--- models/users.go
+++ models/users.go
@@ -18,6 +18,8 @@ var (
 	ErrInvalidID = errors.New("models:ID must be > 0")
 )
 
+const userPwPepper = "secret-random-string"
+
 //NewUserService is
 func NewUserService(connectionInfo string) (*UserService, error) {
 	db, err := gorm.Open("postgres", connectionInfo)
@@ -72,7 +74,8 @@ func first(db *gorm.DB, dst interface{}) error {
 //Create will create the provided user and backfill data
 //like the id,created at,updated at,deleted at
 func (us *UserService) Create(user *User) error {
-	hashedBytes, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	pwBytes := []byte(user.Password + userPwPepper)
+	hashedBytes, err := bcrypt.GenerateFromPassword(pwBytes, bcrypt.DefaultCost)
 	if err != nil {
 		return err
 	}
